2534ecf7480355a85823106d52d5ca10ab51f554
Sanchit Chawla <sanchit_sid@hotmail.com>
2017-01-30 22:47:58 +0700

    Second attempt

--- a2/vert_hist.c
+++ a2/vert_hist.c
@@ -1,10 +1,11 @@
-# include <stdio.h>
-# include <ctype.h> 
-# include <string.h>
+// # include <stdio.h>
+// # include <ctype.h> 
+// # include <string.h>
 
-// int* count_each(char *input){
 
-// 	int* occur[256] = {0};
+// void count_each(char* input, int* occur){
+	
+// 	//char hist[27];
 // 	int i;
 // 	size_t size = strlen(input);
 
@@ -12,32 +13,45 @@
 // 		if (isalpha(input[i]))
 // 			occur[(int) (input[i])]++;
 // 	}
-
-// 	return occur;
-
+	
+// 	printf("%d\n", *occur);
 // }
 
-void count_each(char* input, int* occur){
-	
-	//char hist[27];
-	int i;
-	size_t size = strlen(input);
 
-	for (i=0, i = size - 1; i++;){
-		if (isalpha(input[i]))
-			occur[(int) (input[i])]++;
+// int main(){
+
+// 	int occur[26] = {0};
+// 	char input[256];
+// 	fgets(input, sizeof(input), stdin);
+// 	count_each(input,occur);
+
+// 	return 0;
+// }
+
+# include <stdio.h>
+
+void count_each(char *input, int *occur){
+
+	for (;*input;input++){
+		occur[*input]++;
+
 	}
-	printf("%d\n", *occur);
 }
 
 int main(){
 
-	int occur[26] = {0};
+	int i;
+	int occur[256] = {0};
 	char input[256];
 	fgets(input, sizeof(input), stdin);
 	count_each(input,occur);
 
+	for (i = 0; i = 26; i++){
+		printf("%d\n", occur[i]);
+	} 
+
+	printf("%d\n", sizeof(occur));
+	printf("%s\n", input);
+
 	return 0;
-
-
 }
