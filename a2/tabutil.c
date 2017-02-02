cfe13b901c49ff3e0d524e070142d8e143a7ee84
Sanchit Chawla <sanchit_sid@hotmail.com>
2017-02-02 19:52:08 +0700

    Done with half 0f 2

--- a2/tabutil.c
+++ a2/tabutil.c
@@ -1,16 +1,55 @@
 # include <stdio.h>
 # include <string.h>
 # include <ctype.h>
+# include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
+
+void tab_spaces(int n) {
+	char in = getchar();
+	int i=0;
+    while (in!= EOF) {
+        if (in == '\t') {
+            while (i < n) {
+                putchar(' ');
+                i++;
+            }
+            i = 0;
+        } else {
+            putchar(in);
+        }
+        in = getchar();
+    }
+
+}
+
+void spaces_tab(){
+	char in = getchar();
+	int i=0;
+    while (in!= EOF) {
+        if (in == '\t') {
+            while (i < n) {
+                putchar(' ');
+                i++;
+            }
+            i = 0;
+        } else {
+            putchar(in);
+        }
+        in = getchar();
+    }
+	
+}
+
+int main(int argc, char const *argv[]){
+
+	int n = atoi(argv[2]);
 	
 	if (strcmp(argv[1], "-d") ==0){
-		printf("Do -d");
+		tab_spaces(n);
 	}
-
-	if (strcmp(argv[1], "-e") ==0){
+	else if (strcmp(argv[1], "-e") ==0){
 		printf("Do -e");
 	}
+
 	return 0;
 }
